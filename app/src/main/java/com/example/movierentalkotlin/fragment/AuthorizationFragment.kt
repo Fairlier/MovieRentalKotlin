53072011c0b364692c8b98bbe5f2f963a8e28d44
Fairlier <fairlierprogrammer@gmail.com>
2025-01-20 00:30:10 +0500

    New commit

--- app/src/main/java/com/example/movierentalkotlin/fragment/AuthorizationFragment.kt
+++ app/src/main/java/com/example/movierentalkotlin/fragment/AuthorizationFragment.kt
@@ -8,9 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.movierentalkotlin.databinding.FragmentAuthorizationBinding
 import com.example.movierentalkotlin.util.Constants
